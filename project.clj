2a8816f860cce396a96da5be0b44e64b81f3f3ea
OJIMA Ryoji <rojima1@gmail.com>
2015-06-03 20:10:45 +0900

    twin-spar version is updated.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
                  :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure         "1.6.0"]
                  [clj-time                    "0.9.0"]
-                 [com.tail-island/twin-spar   "0.1.2"]
+                 [com.tail-island/twin-spar   "0.1.3"]
                  [com.tail-island/dog-mission "0.1.0"]])
